3f023628c83eee514b44db5a8514dfa1390faed9
dr-offig <dr.offig@offig.net>
2019-04-14 09:24:16 +0200

    --

--- inst/htmlwidgets/videoR.js
+++ inst/htmlwidgets/videoR.js
@@ -406,18 +406,6 @@ HTMLWidgets.widget({
       ctx.fillStyle = 'bisque';
       ctx.fillRect(0,0,width,height);
 
-      // blue line at playhead
-      const playheadPos = width * curtime / totaltime;
-      ctx.fillStyle = 'cornflowerblue';
-      ctx.fillRect(playheadPos-3, 0, 6, height);
-
-      // print out the current time
-      const curtimeStr = formatTime(curtime,":");
-      ctx.font = '26px serif';
-      const curtimeMet = ctx.measureText(curtimeStr);
-      //const curSpareVert = height - curtimeMet.fontBoundingBoxAscent;
-      ctx.fillText(curtimeStr, Math.max(0, Math.min(width-curtimeMet.width, playheadPos + 6)), height - 4);
-
       // red lines at markers
       const marker_times = markers.time
       function drawMarkerLine(marker_time) {
@@ -431,14 +419,14 @@ HTMLWidgets.widget({
       // If hovering over marker readout its time
       if (isHovering) {
         const marker_time = marker_times.closestTo(whereHovering * totaltime);
-        if ((Math.abs(marker_time - whereHovering * totaltime) / totaltime) < 0.01) {
+        //if ((Math.abs(marker_time - whereHovering * totaltime) / totaltime) < 0.01) {
           const hoverMarkerPos = width * marker_time / totaltime;
           const marktimeStr = formatTime(marker_time,":");
           const marktimeMet = ctx.measureText(marktimeStr);
           //const markSpareVert = height - marktimeMet.fontBoundingBoxAscent;
           //ctx.fillText(marktimeStr, Math.max(0, Math.min(width-marktimeMet.width, hoverMarkerPos - marktimeMet.width/2)), height - (markSpareVert / 2));
           ctx.fillText(marktimeStr, Math.max(0, Math.min(width-marktimeMet.width, hoverMarkerPos + 6)), height - 27);
-        }
+        //}
       }
 
       // If video is seeking, then print a message to that effect on the scrubber
@@ -449,6 +437,19 @@ HTMLWidgets.widget({
         const bufstrMet = ctx.measureText(bufferingStr);
         ctx.fillText(bufferingStr, (width / 2) - bufstrMet.width / 2, height - 10);
       }
+
+      // blue line at playhead
+      const playheadPos = width * curtime / totaltime;
+      ctx.fillStyle = 'cornflowerblue';
+      ctx.fillRect(playheadPos-3, 0, 6, height);
+
+      // print out the current time
+      const curtimeStr = formatTime(curtime,":");
+      ctx.font = '26px serif';
+      const curtimeMet = ctx.measureText(curtimeStr);
+      //const curSpareVert = height - curtimeMet.fontBoundingBoxAscent;
+      ctx.fillText(curtimeStr, Math.max(0, Math.min(width-curtimeMet.width, playheadPos + 6)), height - 4);
+
 
     }
 
